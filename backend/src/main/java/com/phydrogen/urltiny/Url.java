7a641df4b782f61e7e01df4b4d01a00084eb6c2a
Phydrogen <coeffiergabriel@gmail.com>
2023-11-21 22:48:09 +0100

    feat: added getter and constructor

--- backend/src/main/java/com/phydrogen/urltiny/Url.java
+++ backend/src/main/java/com/phydrogen/urltiny/Url.java
@@ -2,10 +2,27 @@ package com.phydrogen.urltiny.data;
 
 public class Url {
 
+    @Id
     private String id;
 
     private String longUrl;
 
     private String shortUrl;
 
+    public Url(String longUrl, String shortUrl) {
+        this.longUrl = longUrl;
+        this.shortUrl = shortUrl;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getLongUrl() {
+        return longUrl;
+    }
+
+    public String getShortUrl() {
+        return shortUrl;
+    }
 }
