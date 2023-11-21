5d50d5e3a056b17fe07bd4879cd062c614919eeb
Phydrogen <coeffiergabriel@gmail.com>
2023-11-21 19:30:06 +0100

    feat: add spring boot

---
+++ src/main/java/com/phydrogen/urltiny/UrltinyApplication.java
@@ -0,0 +1,13 @@
+package com.phydrogen.urltiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrltinyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrltinyApplication.class, args);
+	}
+
+}
