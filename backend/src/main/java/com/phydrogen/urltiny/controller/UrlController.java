d1d452fcf5ccf433ebfc6ae7226f51a1d4bad3a8
Phydrogen <coeffiergabriel@gmail.com>
2023-11-22 11:34:59 +0100

    feat: renamed todo to url

--- backend/src/main/java/com/phydrogen/urltiny/controller/UrlController.java
+++ backend/src/main/java/com/phydrogen/urltiny/controller/UrlController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/todo")
+@RequestMapping("/api/url")
 
 public class UrlController {
     @Autowired
