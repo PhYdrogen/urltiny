d056753334394db73c71990e4df39f5965c49f48
Phydrogen <coeffiergabriel@gmail.com>
2023-11-23 09:32:03 +0100

    feat: create url-form

---
+++ frontend/src/app/url-form/url-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-url-form',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './url-form.component.html',
+  styleUrl: './url-form.component.css'
+})
+export class UrlFormComponent {
+
+}
