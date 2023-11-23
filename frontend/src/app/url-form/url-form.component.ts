b922ca21d922f0c5178e2db443015c495190b41d
Phydrogen <coeffiergabriel@gmail.com>
2023-11-23 12:06:32 +0100

    feat: added more logic

--- frontend/src/app/url-form/url-form.component.ts
+++ frontend/src/app/url-form/url-form.component.ts
@@ -1,21 +1,31 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
+import { FormsModule, NgForm } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { PostResUrl } from '../post-res-url';
+import { ClipboardModule } from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-url-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, MatIconModule, ClipboardModule],
   templateUrl: './url-form.component.html',
   styleUrl: './url-form.component.css'
 })
 export class UrlFormComponent {
-  
+  output = "";
+  longUrl = "";
+  submitted = false;
   constructor(private http: HttpClient) {}
 
-  submit() {
-    this.http.get("http://localhost:8080/api/url").subscribe((data => {
+  submit(form: NgForm) {
+    console.log(form.value);
+    this.http.post("http://localhost:8080/api/url", {"longUrl": form.value.longUrl}).subscribe((data => {
       console.log(data);
+      let dat = data as PostResUrl;
+      this.output = "http://" + "localhost:8080" + "/r/" + dat.shortUrl;
+      this.submitted = true;
     }))
   }
   
